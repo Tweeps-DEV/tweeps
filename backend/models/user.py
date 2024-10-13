3c1ff836ac7eaa482f02aac37d922209100ad9a0
Nile Okomo <okomonile3@gmail.com>
2024-10-13 14:52:52 +0300

    Add user model

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """Defines the User model"""
-from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """Defines a User model. Inherits from Base
     and BaseModel
     """
@@ -15,4 +14,6 @@ class User(BaseModel, Base):
 
     username: Mapped[str] = mapped_column(String(30))
     email: Mapped[str] = mapped_column(String(60))
+    phone_contact: Mapped[str] = mapped_column(String(15))
     password_hash: Mapped[str] = mapped_columns(String(60))
+    address: Mapped[str] = mapped_column(String(20))
