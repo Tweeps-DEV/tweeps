14ca15bd23171f3e0d457e39911117293fda82d7
Nile Okomo <okomonile3@gmail.com>
2024-10-15 11:26:47 +0300

    Add unique and nullability constraints to username and email

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Defines the User model"""
-from models.base_model import BaseModel
+from .base_model import BaseModel
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class User(BaseModel):
@@ -12,8 +11,9 @@ class User(BaseModel):
     """
     __tablename__ = "users"
 
-    username: Mapped[str] = mapped_column(String(30))
-    email: Mapped[str] = mapped_column(String(60))
+    username: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(60),  unique=True, nullable=False)
     phone_contact: Mapped[str] = mapped_column(String(15))
-    password_hash: Mapped[str] = mapped_columns(String(60))
-    address: Mapped[str] = mapped_column(String(20))
+    password_hash: Mapped[str] = mapped_columns(String(60), nullable=False)
+    address: Mapped[str] = mapped_column(String(100))
+    is_admin: Mapped[bool] = mpped_column(default=False)
