135af94fa67a06a3adfff23ca9789a3f91031cd7
Nile Okomo <okomonile3@gmail.com>
2024-10-18 14:32:00 +0300

    Update Order to inherit from BaseModel

--- backend/models/order.py
+++ backend/models/order.py
@@ -1,19 +1,18 @@
 #!usr/bin/env python3
 """Defines an Order model"""
+from .basemodel import BaseModel
 from app import db
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
-class Order(db.Model):
+class Order(BaseModel):
     """
     Represents an order in the system.
 
     This class defines the structure of Order objects and their relationship to Users.
 
     Attributes:
-        id (UUID): The unique identifier for the order, stored as a UUID.
-        user_id (UUID): The UUID of the user who placed the order.
+        user_id (str): The UUID of the user who placed the order.
         date (datetime): The date and time when the order was placed.
         total (float): The total amount of the order.
         status (str): The current status of the order.
@@ -23,7 +22,6 @@ class Order(db.Model):
 
     __tablename__ = "orders"
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
     date = db.Column(db.DateTime, nullable=False)
     total = db.Column(db.Float, nullable=False)
