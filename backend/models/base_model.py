66254f72cbfe94fa3607f8d5fe4ccaf1ec9a14a0
Nile Okomo <okomonile3@gmail.com>
2024-11-08 23:40:36 +0300

    Update basemodel

--- backend/models/base_model.py
+++ backend/models/base_model.py
@@ -19,8 +19,13 @@ class BaseModel(db.Model):
 
     __abstract__ = True
 
-    id = db.Column(db.String(40), primary_key=True, default=lambda: str(uuid.uuid4()), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    id = db.Column(db.String(40),
+                   primary_key=True,
+                   default=lambda: str(uuid.uuid4()),
+                   unique=True,
+                   nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 
     def __init__(self, **kwargs):
@@ -61,11 +66,11 @@ class BaseModel(db.Model):
 
     def to_dict(self):
         """Convert the record to a dictionary."""
-        created_at = self.created_at.isoformat() if self.created_at else None
-        updated_at = self.updated_at.isoformat() if self.updated_at else None
+        created_at = self.created_at.isoformat()
+        updated_at = self.updated_at.isoformat()
 
         return {
             "id": self.id,
-            "created_at": created_at,
-            "updated_at": updated_at,
+            "created_at": created_at if self.created_at else None,
+            "updated_at": updated_at if self.updated_at else None,
         }
