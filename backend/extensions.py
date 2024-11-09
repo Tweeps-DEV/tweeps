34a95dd6d5fe9f1f183269a95bedcdf197c31517
Nile Okomo <okomonile3@gmail.com>
2024-11-10 02:44:30 +0300

    Add backend storage for rate limiter

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
+"""Initializes application extensions"""
+import os
+import redis
 from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_sqlalchemy import SQLAlchemy
 
 bcrypt = Bcrypt()
-db = SQLAlchemy()
-limiter = Limiter(key_func=get_remote_address)
+
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+
+pool = redis.connection.BlockingConnectionPool.from_url(REDIS_URI)
+limiter = Limiter(
+    key_func=get_remote_address,
+    storage_uri=REDIS_URL,
+    strategy='fixed-window',
+    headers_enabled=True,
+    storage_options={'connection_pool': pool},
+)
