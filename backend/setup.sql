52c215072b61e7ec227a779fdebadfba59a7ba22
Nile Okomo <okomonile3@gmail.com>
2024-11-10 02:49:51 +0300

    Add SQL script to set up a testing db

--- backend/setup.sql
+++ backend/setup.sql
@@ -4,3 +4,8 @@ CREATE USER IF NOT EXISTS 'tweeps_dev'@'localhost' IDENTIFIED BY 'tweeps_dev_pwd
 GRANT ALL PRIVILEGES ON tweeps_dev_db.* TO 'tweeps_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'tweeps_dev'@'localhost';
 FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS tweeps_test_db;
+GRANT ALL PRIVILEGES ON tweeps_test_db.* TO 'tweeps_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'tweeps_dev'@'localhost';
+FLUSH PRIVILEGES;
