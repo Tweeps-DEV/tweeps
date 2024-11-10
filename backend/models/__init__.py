0ddcafa98b9e45db764f5af684d9c16085bbbbd7
Nile Okomo <okomonile3@gmail.com>
2024-11-11 02:37:10 +0300

    Update models

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Initialize models package"""
+from models.base_model import BaseModel
 from models.user import User
 from models.menu_item import MenuItem
 from models.cart import Cart
 from models.order import Order
 
-__all__ = ['User', 'MenuItem', 'Cart', 'Order']
+__all__ = ['BaseModel', 'User', 'MenuItem', 'Cart', 'Order']
